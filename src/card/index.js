e0a8860bbc89ae8a6bc423c8e14dbdf636f8fe23
Dan McCurley <djmccurley@gmail.com>
2018-11-27 22:52:13 -0600

    both hovers working and gif tag styles

--- src/card/index.js
+++ src/card/index.js
@@ -28,13 +28,21 @@ const contentStyle = {
     position: 'absolute',
     top: 0,
     width: '90%',
-    height: '90%',
+    height: '75%',
     background: 'rgba(255, 255, 255, 0.9)',
     padding: 5
 };
 
 const contentHeaderStyle = {
     fontSize: 12
+};
+
+const gifTagStyle = {
+    position: 'absolute',
+    top: 12,
+    right: 15,
+    fontWeight: 'bolder',
+    opacity: 0.6
 };
 
 const GifTag = (props) => {
@@ -44,7 +52,7 @@ const GifTag = (props) => {
     }
 
     return (
-        <span>[GIF]</span>
+        <span style={props.style}>[GIF]</span>
     )
 }
 
@@ -56,16 +64,25 @@ class Card extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            isHovered: false
+            isHovered: false,
+            isGif: false
         };
     }
 
     handleEnter = () => {
-        this.setState({isHovered: true});
+        this.setState({ isHovered: true });
     }
 
     handleLeave = () => {
-        this.setState({isHovered: false});
+        this.setState({ isHovered: false });
+    }
+
+    playPic = () => {
+        this.setState({ isGif: true });
+    }
+
+    stopPic = () => {
+        this.setState({ isGif: false });
     }
 
     render() {
@@ -76,16 +93,16 @@ class Card extends Component {
                 style={cardStyle}
             >
                 <div
-                    
-                    onMouseEnter={this.handleEnter}
-                    onMouseLeave={this.handleLeave}
+                    onMouseEnter={this.playPic}
+                    onMouseLeave={this.stopPic}
+
 
                 >
                     <div>
-                        <img src={this.props.shotinfo.images.teaser}></img>
-                        <GifTag animated={this.props.shotinfo.animated} />
+                        <img src={!this.state.isGif ? this.props.shotinfo.images.teaser : this.props.shotinfo.images.hidpi}></img>
+                        <GifTag style={gifTagStyle} animated={this.props.shotinfo.animated} />
                     </div>
-                    <div style={contentHidden}>
+                    <div style={!this.state.isHovered ? contentHidden : contentStyle}>
                         <h2 style={contentHeaderStyle}>{this.props.shotinfo.title}</h2>
                         <div dangerouslySetInnerHTML={createMarkup(this.props)} />
                         <p><Timestamp time={this.props.shotinfo.created_at} format="date" /></p>
@@ -93,7 +110,13 @@ class Card extends Component {
                     </div>
 
                 </div>
-                <Footer props={{ views: this.props.shotinfo.views_count, likes: this.props.shotinfo.likes_count, comments: this.props.shotinfo.comments_count }} />
+                <div
+                    style={{display: 'flex', flexWrap: 'wrap'}}
+                    onMouseEnter={this.handleEnter}
+                    onMouseLeave={this.handleLeave}
+                >
+                    <Footer props={{ views: this.props.shotinfo.views_count, likes: this.props.shotinfo.likes_count, comments: this.props.shotinfo.comments_count }} />
+                </div>
             </div>
         )
     }
